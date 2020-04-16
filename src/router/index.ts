0ca83a25be159e716805e4679b7cd0f7e7f0d4e4
cornercrap1103 <hikarynogo113@gmail.com>
2020-04-16 22:53:10 +0900

    Add base component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,23 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
+    name: "Base",
+    component: () => import("../views/Base.vue")
+  },
+  {
+    path: "/home",
     name: "Home",
-    component: Home
+    component: () => import("../views/Home.vue")
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import("../views/About.vue")
   }
 ];
 
