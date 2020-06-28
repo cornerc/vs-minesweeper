4fc671a611f3dac42584e1f389222a6b0610f8be
cornercrap <hikarynogo113@gmail.com>
2020-06-28 13:41:21 +0900

    add packeage cli-plugin-unit-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,11 @@ module.exports = {
   root: true,
   extends: ["plugin:vue/recommended", "@vue/prettier", "@vue/typescript"],
   parser: "vue-eslint-parser",
+
   parserOptions: {
     parser: "@typescript-eslint/parser",
   },
+
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
@@ -16,4 +18,16 @@ module.exports = {
       },
     ],
   },
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
